9f4c54680680af131e03968dd336478b5a8db0ef
ParkSunghwa <mtreeferryp@gmail.com>
2022-05-31 16:28:08 +0900

    220531

--- js/script.js
+++ js/script.js
@@ -6,10 +6,33 @@ $(document).ready(function(){
 
     });
 
+    // .wrap_layout_background start
+
+    $(".wrap_layout_background > .box_layout_background > .box_language > .language").click(function(){
+        
+        $(this).addClass("active");
+        $(this).siblings().removeClass("active");
+
+        let korean = $(".korean").hasClass("active");
+        let english = $(".english").hasClass("active");
 
 
+        if(korean == true){
+        $(".kor").addClass("active");
+        $(".eng").removeClass("active");
+        } else if(english == true){
+        $(".eng").addClass("active");
+        $(".kor").removeClass("active");
+        };
+    });
 
+    // .wrap_layout_background end
 
+    // .box_introduction start
+
+    
+
+    // .box_introduction end
 
 
 
