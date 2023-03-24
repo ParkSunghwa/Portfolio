a68f7f9d37edba1328f2e2ed7f06a1c851647fb3
ParkSunghwa <mtreeferryp@gmail.com>
2023-03-24 20:47:33 +0900

    230324

---
+++ js/projectList.js
@@ -0,0 +1,88 @@
+const projectList = [{
+        id: 0,
+        title: {
+            korean: '도서검색대',
+            english: 'Book Browser',
+        },
+        goal: {
+            korean: 'Node.js 연습',
+            english: 'Node.js Practice',
+        },
+        concept: {
+            korean: ['서버제작', '계정접속', 'API'],
+            english: ['Server', 'account', 'API'],
+        },
+        link: "https://parksunghwa.du.r.appspot.com/",
+        image: 'bookbrowser.png',
+    },
+    {
+        id: 1,
+        title: {
+            korean: '신발 쇼핑몰',
+            english: 'Shoe Store',
+        },
+        goal: {
+            korean: 'React 연습',
+            english: 'React Practice',
+        },
+        concept: {
+            korean: ['리액트 라우터', '세션스토리지', '부트스트랩'],
+            english: ['React-Router', 'Session Storage', 'Bootstrap'],
+        },
+        link: "https://parksunghwa.github.io/react_shoestore/",
+        image: 'shoestore.png',
+    },
+    {
+        id: 2,
+        title: {
+            korean: '글로리어스워커',
+            english: 'gloriousworker',
+        },
+        goal: {
+            korean: '퍼블리싱 실무',
+            english: 'Practical Publishing',
+        },
+        concept: {
+            korean: ['카페24', 'PC&모바일'],
+            english: ['Cafe24', 'PC&Mobile'],
+        },
+        link: "https://gloriousworker.co/",
+        image: 'gloriousworker.png',
+    },
+    {
+        id: 3,
+        title: {
+            korean: '가구 쇼핑몰',
+            english: 'Interior Store',
+        },
+        goal: {
+            korean: '자바스크립트 연습',
+            english: 'Javascript Practice',
+        },
+        concept: {
+            korean: ['드래그', '버튼액션', '부트스트랩'],
+            english: ['Draggable', 'Button Action', 'Bootstrap'],
+        },
+        link: "https://parksunghwa.github.io/jsstudy_shoppingmall/",
+        image: 'interior.png',
+    },
+    {
+        id: 4,
+        title: {
+            korean: '오키친',
+            english: 'O Kitchen',
+        },
+        goal: {
+            korean: '리디자인',
+            english: 'Re-design',
+        },
+        concept: {
+            korean: ['반응형', '전체화면'],
+            english: ['Reative', 'Fullpage'],
+        },
+        link: "https://parksunghwa.github.io/redesign_o-kitchen/",
+        image: 'okitchen.png',
+    },
+];
+
+export default projectList;
--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-$(document).ready(function(){
+import projectList from './projectList.js';
 
+$(document).ready(function(){
     $(".wrap_fullpage").fullpage({
 
         navigation:true,
+        touchSensitivity:10,
         
         // .box_ability start
         afterLoad:function(origin, destination, direction){
@@ -59,6 +61,51 @@ $(document).ready(function(){
 
     
     // .box_project start
+
+    projectList.forEach(function(a,i){
+        var templateConceptPCKor = '', templateConceptPCEng = '', templateConceptKor = '', templateConceptEng = '';
+        a.concept.korean.forEach(function(b,i){
+            templateConceptPCKor = templateConceptPCKor+b+'<br/>';
+        });
+        a.concept.english.forEach(function(b,i){
+            templateConceptPCEng = templateConceptPCEng+b+'<br/>';
+        });
+        a.concept.korean.forEach(function(b,i){
+            templateConceptKor = templateConceptKor+b+'&#32;';
+        });
+        a.concept.korean.forEach(function(b,i){
+            templateConceptEng = templateConceptEng+b+'&#32;';
+        });
+        
+        $(`.view_pc>.box_project>.box_layout_content>.box_content.left>.box_cover_slide>.box_slide .content_slide.slide0${i+1}>.box_image`).css({"background-image" : `url(../image/03project/${a.image})`});
+        $(`.view_tab>.box_project_tab>.box_layout_content>.box_content.left>.box_cover_slide>.box_slide .content_slide.slide0${i+1}`).css({"background-image" : `url(../image/03project/${a.image})`});
+        $(`.view_mo>.box_project_mo>.box_layout_content>.box_content.left>.box_cover_slide>.box_slide .content_slide.slide0${i+1}`).css({"background-image" : `url(../image/03project/${a.image})`});
+
+        $(`.view_pc>.box_project>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.kor>.box_text.text01>.box_explanation_2depth>.text_explanation_2depth`).text(a.title.korean);
+        $(`.view_pc>.box_project>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.kor>.box_text.text02>.box_explanation_2depth>.text_explanation_2depth`).text(a.goal.korean);
+        $(`.view_pc>.box_project>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.kor>.box_text.text03>.box_explanation_2depth>.text_explanation_2depth`).html(templateConceptPCKor);
+        $(`.view_pc>.box_project>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.eng>.box_text.text01>.box_explanation_2depth>.text_explanation_2depth`).text(a.title.english);
+        $(`.view_pc>.box_project>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.eng>.box_text.text02>.box_explanation_2depth>.text_explanation_2depth`).text(a.goal.english);
+        $(`.view_pc>.box_project>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.eng>.box_text.text03>.box_explanation_2depth>.text_explanation_2depth`).html(templateConceptPCEng);
+
+        $(`.view_tab>.box_project_tab>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.kor>.box_text.text01>.box_explanation_2depth>.text_explanation_2depth`).text(a.title.korean);
+        $(`.view_tab>.box_project_tab>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.kor>.box_text.text02>.box_explanation_2depth>.text_explanation_2depth`).text(a.goal.korean);
+        $(`.view_tab>.box_project_tab>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.kor>.box_text.text03>.box_explanation_2depth>.text_explanation_2depth`).html(templateConceptKor);
+        $(`.view_tab>.box_project_tab>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.eng>.box_text.text01>.box_explanation_2depth>.text_explanation_2depth`).text(a.title.english);
+        $(`.view_tab>.box_project_tab>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.eng>.box_text.text02>.box_explanation_2depth>.text_explanation_2depth`).text(a.goal.english);
+        $(`.view_tab>.box_project_tab>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.eng>.box_text.text03>.box_explanation_2depth>.text_explanation_2depth`).html(templateConceptEng);
+        
+        $(`.view_mo>.box_project_mo>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.kor>.box_text.text01>.box_explanation_2depth>.text_explanation_2depth`).text(a.title.korean);
+        $(`.view_mo>.box_project_mo>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.kor>.box_text.text02>.box_explanation_2depth>.text_explanation_2depth`).text(a.goal.korean);
+        $(`.view_mo>.box_project_mo>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.kor>.box_text.text03>.box_explanation_2depth>.text_explanation_2depth`).html(templateConceptKor);
+        $(`.view_mo>.box_project_mo>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.eng>.box_text.text01>.box_explanation_2depth>.text_explanation_2depth`).text(a.title.english);
+        $(`.view_mo>.box_project_mo>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.eng>.box_text.text02>.box_explanation_2depth>.text_explanation_2depth`).text(a.goal.english);
+        $(`.view_mo>.box_project_mo>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_cover.eng>.box_text.text03>.box_explanation_2depth>.text_explanation_2depth`).html(templateConceptEng);
+
+        $(`.view_pc>.box_project>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_outerlink>.outerlink`).attr('href',a.link);
+        $(`.view_tab>.box_project_tab>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_outerlink>.outerlink`).attr('href',a.link);
+        $(`.view_mo>.box_project_mo>.box_layout_content>.box_content.right>.box_projectinfo.project0${i+1}>.box_outerlink>.outerlink`).attr('href',a.link);
+    });
 
     const swiper = new Swiper(".view_pc .swiper", {
 
